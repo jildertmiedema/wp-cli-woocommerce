7a8a7a26377bad97b6ce40e153f8d63588bbeb27
Jildert Miedema <jildert@pimenta.nl>
2013-09-09 16:11:32 +0200

    install woocommerce via cli

--- src/Woocommerce-Command.php
+++ src/Woocommerce-Command.php
@@ -14,13 +14,25 @@ class Woocommerce_Command extends WP_CLI_Command {
 	 * 
 	 *     wp woocommerce install
 	 *
-	 * @synopsis <name>
+	 * @synopsis 
 	 */
 	function install( $args, $assoc_args ) {
-		list( $name ) = $args;
+		if (!is_plugin_active ( 'woocommerce/woocommerce.php') ){
+			WP_CLI::error('Woocommerce is not installed');
+		}
 
-		// Print a success message
-		WP_CLI::success( "Hello, $name!" );
+		if (get_option('_wc_needs_pages')) {
+
+			require_once(WP_PLUGIN_DIR. '/woocommerce/admin/woocommerce-admin-install.php' );
+			woocommerce_create_pages();
+
+			delete_option( '_wc_needs_pages' );
+			delete_transient( '_wc_activation_redirect' );
+
+			WP_CLI::success( "Installed woocommerce" );
+		} else {
+			WP_CLI::warning( 'Woocommerce already seems to be installed');
+		}
 	}
 }
 
